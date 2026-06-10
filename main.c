ee001eb494f941627a877a5b26de05abccd1fa93
tungsteno <davide.torregrossa.pvt@gmail.com>
2026-06-10 21:45:22 +0200

    second commit

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 #include <stdio.h>
 
-//-----------------------------------print_chars.c
-int print_char(char c) {	
-	write(1, &c, 1);
-	return 1;
-}
+//-----------------------------------print_words.c
+//int print_char(char c) {	
+//	write(1, &c, 1);
+//	return 1;
+//}
 
 int print_string(char *str) {
 	size_t	i;
@@ -19,16 +19,19 @@ int print_string(char *str) {
 }
 
 //------------------------------print_ptr.c
-static int	print_hex(unsigned long n)
+static int	print_hex(unsigned long n, int upper)
 {
 	int				out;
 	char			*ref;
 	char			c;
 	
 	out = 0;
-	ref = "0123456789abcdef";
+	if (upper)
+		ref = "0123456789ABCDEF";
+	else
+		ref = "0123456789abcdef";
 	if (n >= 16)
-		out += print_hex(n/16);
+		out += print_hex(n/16, upper);
 	c = ref[n % 16];
 	out += write(1, &c, 1);
 	return out;
@@ -43,7 +46,7 @@ int print_ptr(void *ptr) {
 		return (write(1, "(nil)", 5));
 	n = (unsigned long)ptr;
 	out += write(1, "0x", 2);
-	out += print_hex(n);
+	out += print_hex(n, 0);
 	return out;
 }
 
@@ -55,7 +58,7 @@ static int print_num(long n) {
 
 	out = 0;
 	if (n > 9)
-		out += ft_putnbr_base(n / 10);
+		out += print_num(n / 10);
 	c = (n % 10) + '0';
 	out += write(1, &c, 1);
 	return (out);
@@ -63,7 +66,7 @@ static int print_num(long n) {
 
 int print_uint(unsigned int n)
 {
-	return (print_num((long)n);
+	return print_num((long)n);
 }
 
 
@@ -89,11 +92,62 @@ int main() {
 	int posn = 30;
 	char *str = "ciao!";
 	char *ptr = "ptr!";
-		
 
+	printf("d---------------------------------");
+	printf("original: \n");
+	printf(" returns %d\n", printf("%d", negn));
+	printf("mine: \n");
+	printf(" returns %d\n\n", print_dec(negn));
 
-	//char *ptr = "ciao";
-	//int amt = print_ptr(&ptr);
-	//printf("\n%d", amt);
+	printf("original: \n");
+	printf(" returns %d\n", printf("%d", posn));
+	printf("mine: \n");
+	printf(" returns %d\n\n", print_dec(posn));
 
+	printf("s---------------------------------");
+	printf("original: \n");
+	printf(" returns %d\n", printf("%s", str));
+	printf("mine: \n");
+	printf(" returns %d\n\n", print_string(str));
+	
+	printf("c---------------------------------");
+	printf("original: \n");
+	printf(" returns %d\n", printf("%c", 'c'));
+	printf("mine: \n");
+	printf(" returns %d\n\n", print_string((char[]){'c', 0}));
+	
+	printf("p---------------------------------");
+	printf("original: \n");
+	printf(" returns %d\n", printf("%p", &str));
+	printf("mine: \n");
+	printf(" returns %d\n\n", print_ptr(&str));
+
+	printf("i---------------------------------");
+	printf("original: \n");
+	printf(" returns %d\n", printf("%i", posn));
+	printf("mine: \n");
+	printf(" returns %d\n\n", print_dec(posn));
+
+	printf("original: \n");
+	printf(" returns %d\n", printf("%i", negn));
+	printf("mine: \n");
+	printf(" returns %d\n\n", print_dec(negn));
+	
+	printf("u---------------------------------");
+	printf("original: \n");
+	printf(" returns %d\n", printf("%u", posn));
+	printf("mine: \n");
+	printf(" returns %d\n\n", print_dec(posn));
+
+	printf("x---------------------------------");
+	printf("original: \n");
+	printf(" returns %d\n", printf("%x", 255));
+	printf("mine: \n");
+	printf(" returns %d\n\n", print_hex(255, 0));
+
+	printf("X---------------------------------");
+	printf("original: \n");
+	printf(" returns %d\n", printf("%X", 255));
+	printf("mine: \n");
+	printf(" returns %d\n\n", print_hex(255, 1));
 }
